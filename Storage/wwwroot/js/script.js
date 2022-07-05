44c0e55db2d858e29262230f7e5d4e286e5993e7
Mikael Klevengård <mickekleven@outlook.com>
2022-07-05 13:01:50 +0200

    Remove ajax jquery function from script.js

--- Storage/wwwroot/js/script.js
+++ Storage/wwwroot/js/script.js
@@ -7,34 +7,6 @@ btnFetch.addEventListener('click', function () {
 });
     
 
-
-
-$(document).ready(function () {
-
-    $('#btnprodlist').click(function () {
-
-        let qs = $('#inpprodlist').val();
-
-        if (qs === null || qs === undefined) { alert('Input is null'); }
-
-        $.ajax({
-            type: 'GET',
-            url: '/Products/FindByOptionAjax',
-            data: { findOption: qs },
-            contentType: "text",
-            dataType: "json",
-            success: function (result) {
-                //return result;
-                //$('#prodlistresult').empty();
-                $('#prodlistresult').html(result);
-                console.log(result);
-            }
-        });
-    });
-});
-
-
-
 const fetchProductList = function () {
 
     let request = document.querySelector('#prodlist');
